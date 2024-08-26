0b35c7f609d6c82408200518615527fa266d0bbe
seongyeon1 <ksy974498@gmail.com>
2024-08-27 01:01:02 +0900

    end

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # app/main.py
 
 from app.services.dialogue_extraction import extract_dress_info
-from app.services.dress_generation import generate_dress
+from app.services.dress_generation import make_prompt, generate_dress
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -10,13 +10,20 @@ app = FastAPI()
 class DialogueRequest(BaseModel):
     dialogue: str
 
+@app.post("/extract-dress-feature/")
+async def generate_dress_from_dialogue(request: DialogueRequest):
+    try:
+        dress_info = extract_dress_info(request.dialogue)
+        return dress_info
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 @app.post("/generate-dress/")
 async def generate_dress_from_dialogue(request: DialogueRequest):
     try:
-        # Assuming extract_dress_info and generate_dress are defined somewhere in your service
-        dress_prompt = extract_dress_info(request.dialogue)
+        dress_prompt = make_prompt(request.dialogue)
         dress_image = generate_dress(dress_prompt)
-
         return dress_image # Returning the image URL or base64 encoded image data
 
     except Exception as e:
